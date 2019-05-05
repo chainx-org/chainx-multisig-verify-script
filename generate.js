19eedd2375007a776ff5b36ae964ff7ee6d0a1ec
Yongfeng LI <wliyongfeng@gmail.com>
2019-05-05 10:55:06 +0800

    Use global network variable in generate script

--- generate.js
+++ generate.js
@@ -1,14 +1,17 @@
 const bitcoin = require("bitcoinjs-lib");
 
-const network = bitcoin.networks.testnet
+const network = bitcoin.networks.testnet;
 async function generate() {
-  const keyPair = bitcoin.ECPair.makeRandom({network, compressed: true});
+  const keyPair = bitcoin.ECPair.makeRandom({ network, compressed: true });
   const wif = keyPair.toWIF();
   const pubkey = Buffer.from(keyPair.publicKey).toString("hex");
-  const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey, network: bitcoin.networks.testnet });
+  const { address } = bitcoin.payments.p2pkh({
+    pubkey: keyPair.publicKey,
+    network
+  });
   console.log(`private key(WIF): ${wif}`);
   console.log(`public key: ${pubkey}`);
   console.log(`address: ${address}`);
 }
 
-generate()
+generate();
